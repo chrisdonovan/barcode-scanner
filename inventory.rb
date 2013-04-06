4f8c47e59f485b939dbc0e84dd632daf8b7e989e
Chris Donovan <chrisdonovan@sub241-125.clayton.edu>
2013-04-06 18:14:34 -0400

    Added update functionality

--- inventory.rb
+++ inventory.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-#Get the command-line argument
+require 'csv'
 
 def helpscrn
 	# define the help variable which holds the help text
@@ -36,18 +36,43 @@ def updateinv
 			puts "\nInvalid file format – unable to proceed."
 			puts "Usage: ruby inventory.rb [?|-h|help|[-u|-o|-z <infile>|[<outfile>]]]"
 		else
-			csv_file = File.open("/Volumes/Chris-Storage/Current Courses/Programming Languages/Homework/Barcode Scanner/" << ARGV[1])
-			if csv_file
-				puts "file found"
-			else
-				puts "Input file #{ARGV[1]} not found - aborting."
-				break
+			filename = "./" << ARGV[1]
+			database_file = "./inventory.accdb"
+
+			# Attempt to open user csv file. If not found, abort program.
+			begin
+				data_file = File.open(database_file)
+			# Instead of asking for new file name, abort if file not found.
+			rescue
+				abort "Database file not found - aborting."
+			end
+
+			csv_input = Array.new{Array.new}
+
+			i = 0
+			data_file.each do |line|
+				csv_input[i] = line.split(",").map(&:strip)
+				i += 1
+			end
+
+
+			# Attempt to open user csv file. If not found, abort program.
+			begin
+				csv_file = CSV.open(filename, "r")
+			# Instead of asking for new file name, abort if file not found.
+			rescue
+				abort "Input file #{ARGV[1]} not found - aborting."
 			end
 			
-			# Update inventory
+			CSV.foreach(filename) do |row|
+				csv_input << row
+			end
 
-			# Update error
-			puts ""
+			CSV.open(database_file, "w") do |csv|
+				csv_input.each do |a|
+					csv << [a[0], a[1], a[2], a[3], a[4], a[5]]
+				end
+			end
 
 			# Update successful
 			puts "Updated #{csv_file.count} database records successfully"
